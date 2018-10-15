749756b55271e0d47c5bf3aff450c5a68fb62ef7
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-15 23:03:43 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = ["snickers", "hundered grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  var array = ["element", ...array]
+  return array
+}
