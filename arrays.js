4f0af3fd9a41ad2c1a1b1c3aa8d2e4d40f421e4f
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-16 02:06:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -16,6 +16,6 @@ function destructivelyAddElementToEndOfArray(array, element) {
   array.push(element)
   return array
 }
-function accessElementInArray(array, element) {
-  console.log(element[index])
+function accessElementInArray(array, index) {
+  console.log(array[index])
 }
